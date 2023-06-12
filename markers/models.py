dea09335200cf3f93dd47a19f410f395b4a28281
laijackylai <66239914+laijackylai@users.noreply.github.com>
2023-06-11 23:45:18 -0400

    del django

--- markers/models.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.gis.db import models
-
-
-class Marker(models.Model):
-    name = models.CharField(max_length=255)
-    location = models.PointField()
-
-    def __str__(self):
-        return self.name
--- markers/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from markers.views import MarkersMapView
-
-app_name = "markers"
-
-urlpatterns = [
-    path("map/", MarkersMapView.as_view()),
-]
