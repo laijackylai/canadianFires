c87e9af37953ac0518965d2876c73388a1bb57d0
laijackylai <66239914+laijackylai@users.noreply.github.com>
2023-06-14 00:05:55 -0400

    fix

---
+++ _next/static/MhdHRQ4Uxr2fcjHRH8uGe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/530-3027c0b036ed3f89.js","static/chunks/pages/index-05afeb71d4bf4136.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
