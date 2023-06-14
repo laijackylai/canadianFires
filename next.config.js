077501097281d88ae4b9913d88bde977dd2f9c6f
laijackylai <66239914+laijackylai@users.noreply.github.com>
2023-06-14 01:26:38 -0400

    fix

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
+// const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: isProd ? '/canadianFires/' : '',
-  images: {
-    unoptimized: true,
-  },
+  // assetPrefix: isProd ? '/canadianFires/' : '',
+  // images: {
+  //   unoptimized: true,
+  // },
 }
 
 module.exports = nextConfig
