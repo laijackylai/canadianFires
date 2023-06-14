8c79a91f47ab68158931f2837874fdfb4406823c
laijackylai <66239914+laijackylai@users.noreply.github.com>
2023-06-14 00:05:55 -0400

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import UI from '@/components/ui';
 import { QueryContextProvider } from '@/contexts/queryDataContext';
 import dynamic from 'next/dynamic';
-import { GetServerSideProps, NextPage } from 'next/types';
+import { NextPage } from 'next/types';
 const Map = dynamic(() => import('@/components/map'), { ssr: false });
 
 type Props = {
@@ -15,14 +15,6 @@ const App: NextPage<Props> = ({ }) => {
       <Map />
     </QueryContextProvider>
   );
-}
-
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  return {
-    props: {
-
-    },
-  };
 }
 
 export default App
